c4803a3960dfbd9beab178874ae03a4ec9861225
Tim Caswell <tim@creationix.com>
2017-10-09 16:37:13 -0500

    Fix large repos

--- dat-route.js
+++ dat-route.js
@@ -1,7 +1,6 @@
 const hyperdrive = require('hyperdrive')
 const swarm = require('hyperdiscovery')
 const hyperdriveHttp = require('hyperdrive-http')
-const ram = require('random-access-memory')
 
 module.exports = datRoute
 
@@ -9,23 +8,37 @@ function datRoute (url, options) {
   let {storage} = options || {}
 
   // We can't serve a dat without knowing it's address!
-  if (!url) throw new TypeError('options.url is a required field.')
+  if (!url) throw new TypeError('url is required')
   let match = url.match(/[0-9a-f]{64}/)
-  if (!match) throw new TypeError('options.url needs to contain 64 hex chars.')
+  if (!match) throw new TypeError('url needs to contain 64 hex chars')
+  let hex = match[0]
 
-  let key = Buffer.from(match[0], 'hex')
+  let key = Buffer.from(hex, 'hex')
 
   // Default to in-memory storage if none is given
-  if (!storage) storage = name => ram()
+  if (!storage) storage = process.env.HOME + '/.dat-route/' + match[0]
 
   // Create a sparse hyperdrive instance.
-  let archive = hyperdrive(storage, key, {sparse: true, sparseMetadata: true})
+  let archive = hyperdrive(storage, key, { sparse: true, sparseMetadata: true })
 
-  // Connect to the DAT P2P network to find peers.
   archive.on('ready', () => {
-    swarm(archive)
+    // Connect to the DAT P2P network to find peers.
+    let sw = swarm(archive, { live: true })
+
+    // Make sure we watch for updates, even in full-sparse mode
+    onUpdate()
+    function onUpdate () { archive.metadata.update(onUpdate) }
+
+    // Log some interesting events for basic diagnostics
+    archive.metadata.on('sync', () => { console.log(url, 'META-SYNC') })
+    archive.metadata.on('append', () => { console.log(url, 'META-APPEND') })
+    sw.once('peer', () => { console.log(url, 'PEER') })
   })
 
   // Use the hyperdrive-http library for the bulk of http handling.
-  return hyperdriveHttp(archive)
+  return hyperdriveHttp(archive, {
+    exposeHeaders: true,
+    live: true,
+    footer: `<a href="dat://${hex}/">Also available over DAT protocol</a>`
+  })
 }
--- server-static.js
+++ server-static.js
@@ -1,20 +1,32 @@
 #!/usr/bin/env node
 const express = require('express')
 const morgan = require('morgan')
+const auth = require('basic-auth')
 const datRoute = require('./dat-route')
 
 const port = process.env.PORT || 8080
 const host = process.env.HOST || 'localhost'
+const username = process.env.USERNAME || 'admin'
+const password = process.env.PASSWORD || 'daplierocks'
 
 let app = express()
 
 app.use(morgan('dev'))
 
 app.use('/exploder/', datRoute(
-  'dat://d13556ea04ee14fd560e456b438153a8ed089efc997118d547f61179e7617448/'))
+  'dat://d13556ea04ee14fd560e456b438153a8ed089efc997118d547f61179e7617448'))
 
 app.use('/conquest/', datRoute(
-  'dat://b1fd8e1d7eb4f85ecf619a149d7105ae1dd27921c12d3049b932115b52f37d81/'))
+  'dat://b1fd8e1d7eb4f85ecf619a149d7105ae1dd27921c12d3049b932115b52f37d81'))
+
+app.use('/isos/', datRoute(
+  'dat://1eb828a5eddfd6f84fb0d40cd217071798c5ff8a92ce9606b0551d312a31aa1e'))
+
+app.use('/music/', requireLogin, datRoute(
+  'dat://***'))
+
+app.use('/movies/', requireLogin, datRoute(
+  'dat://***'))
 
 app.get('/', (req, res) => {
   res.send(`
@@ -30,7 +42,11 @@ app.get('/', (req, res) => {
         <ul>
           <li><a href="/exploder/">Exploder</a></li>
           <li><a href="/conquest/">Conquest</a></li>
+          <li><a href="/isos/">ISOs</a></li>
+          <li><a href="/music/">Music*</a></li>
+          <li><a href="/movies/">Movies*</a></li>
         </ul>
+        <p>* Requires authentication</p>
       </body>
     </html>
   `)
@@ -39,3 +55,13 @@ app.get('/', (req, res) => {
 app.listen({port, host}, function () {
   console.log(`Server listening on http://${host}${port === 80 ? '' : ':' + port}/`)
 })
+
+function requireLogin (req, res, next) {
+  let credentials = auth(req)
+  if (credentials && credentials.name === username && credentials.pass === password) {
+    return next()
+  }
+  res.statusCode = 401
+  res.setHeader('WWW-Authenticate', 'Basic realm="media files"')
+  res.end('Access denied')
+}
