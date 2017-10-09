0eec3481914c8b995eba70b265eb613db3a3993a
Tim Caswell <tim@creationix.com>
2017-10-09 13:21:24 -0500

    Switch to express for sample to reduce scope of dat handler

--- server-static.js
+++ server-static.js
@@ -1,27 +1,23 @@
 #!/usr/bin/env node
-const stack = require('stack')
+const express = require('express')
 const morgan = require('morgan')
 const datRoute = require('./dat-route')
-const { createServer } = require('http')
 
 const port = process.env.PORT || 8080
 const host = process.env.HOST || 'localhost'
 
-createServer(stack(
-  morgan('dev'),
-  serveIndex,
-  datRoute('dat://d13556ea04ee14fd560e456b438153a8ed089efc997118d547f61179e7617448/', {prefix: 'exploder'}),
-  datRoute('dat://b1fd8e1d7eb4f85ecf619a149d7105ae1dd27921c12d3049b932115b52f37d81/', {prefix: 'conquest'})
-)).listen(port)
-console.log(`Server listening on http://${host}${port === 80 ? '' : ':' + port}/`)
+let app = express()
 
+app.use(morgan('dev'))
 
-function serveIndex (req, res, next) {
-  if (req.url !== '/') return next()
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  })
-  res.end(`
+app.use('/exploder/', datRoute(
+  'dat://d13556ea04ee14fd560e456b438153a8ed089efc997118d547f61179e7617448/'))
+
+app.use('/conquest/', datRoute(
+  'dat://b1fd8e1d7eb4f85ecf619a149d7105ae1dd27921c12d3049b932115b52f37d81/'))
+
+app.get('/', (req, res) => {
+  res.send(`
     <!doctype html>
     <html>
       <head>
@@ -38,4 +34,8 @@ function serveIndex (req, res, next) {
       </body>
     </html>
   `)
-}
+})
+
+app.listen({port, host}, function () {
+  console.log(`Server listening on http://${host}${port === 80 ? '' : ':' + port}/`)
+})
